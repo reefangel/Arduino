f6b9e8800b1e0bcc48d15cd33865535d4b525ba9
Roberto Imai <roberto@imlogo.com>
2012-06-08 10:03:56 -0700

    Update feature finalized and external file was created for easy control of files to be updated

--- app/src/processing/app/UpdateCheck.java
+++ app/src/processing/app/UpdateCheck.java
@@ -26,6 +26,11 @@ import java.io.*;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Random;
+import java.io.BufferedReader; 
+import java.io.FileReader; 
+import java.io.IOException; 
+import java.util.ArrayList; 
+import java.util.List; 
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -72,7 +77,6 @@ public class UpdateCheck implements Runnable {
 
 
 	public void run() {
-		
 		try {
 			String lastString = Preferences.get("update.last");
 			long now = System.currentTimeMillis();
@@ -213,118 +217,30 @@ public class UpdateCheck implements Runnable {
 	class DownloadFile implements Runnable
 	{
 		String[] SourceFiles=new String[0];
-		String[] DestFiles=new String[0];
 		
 		public void run(){
-	        AddFile("https://raw.github.com/reefangel/libraries/master/DS1307RTC/DS1307RTC.cpp",Base.getSketchbookFolder().getPath() + "/libraries/DS1307RTC/DS1307RTC.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/DS1307RTC/DS1307RTC.h",Base.getSketchbookFolder().getPath() + "/libraries/DS1307RTC/DS1307RTC.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/DS1307RTC/keywords.txt",Base.getSketchbookFolder().getPath() + "/libraries/DS1307RTC/keywords.txt");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/DS1307RTC/readme.txt",Base.getSketchbookFolder().getPath() + "/libraries/DS1307RTC/readme.txt");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/NokiaLCD/NokiaLCD.cpp",Base.getSketchbookFolder().getPath() + "/libraries/NokiaLCD/NokiaLCD.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/NokiaLCD/NokiaLCD.h",Base.getSketchbookFolder().getPath() + "/libraries/NokiaLCD/NokiaLCD.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/NokiaLCD/README",Base.getSketchbookFolder().getPath() + "/libraries/NokiaLCD/README");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/OneWire/OneWire.cpp",Base.getSketchbookFolder().getPath() + "/libraries/OneWire/OneWire.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/OneWire/OneWire.h",Base.getSketchbookFolder().getPath() + "/libraries/OneWire/OneWire.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/OneWire/keywords.txt",Base.getSketchbookFolder().getPath() + "/libraries/OneWire/keywords.txt");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Phillips6610LCD/Phillips6610LCD.cpp",Base.getSketchbookFolder().getPath() + "/libraries/Phillips6610LCD/Phillips6610LCD.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Phillips6610LCD/Phillips6610LCD.h",Base.getSketchbookFolder().getPath() + "/libraries/Phillips6610LCD/Phillips6610LCD.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Phillips6610LCD/Phillips6610LCD.o",Base.getSketchbookFolder().getPath() + "/libraries/Phillips6610LCD/Phillips6610LCD.o");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Phillips6610LCDInv/Phillips6610LCDInv.cpp",Base.getSketchbookFolder().getPath() + "/libraries/Phillips6610LCDInv/Phillips6610LCDInv.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Phillips6610LCDInv/Phillips6610LCDInv.h",Base.getSketchbookFolder().getPath() + "/libraries/Phillips6610LCDInv/Phillips6610LCDInv.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Time/Time.cpp",Base.getSketchbookFolder().getPath() + "/libraries/Time/Time.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Time/Time.h",Base.getSketchbookFolder().getPath() + "/libraries/Time/Time.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Time/DateStrings.cpp",Base.getSketchbookFolder().getPath() + "/libraries/Time/DateStrings.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Time/keywords.txt",Base.getSketchbookFolder().getPath() + "/libraries/Time/keywords.txt");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Time/Readme.txt",Base.getSketchbookFolder().getPath() + "/libraries/Time/Readme.txt");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/TimeAlarms/TimeAlarms.cpp",Base.getSketchbookFolder().getPath() + "/libraries/TimeAlarms/TimeAlarms.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/TimeAlarms/TimeAlarms.h",Base.getSketchbookFolder().getPath() + "/libraries/TimeAlarms/TimeAlarms.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/TimeAlarms/keywords.txt",Base.getSketchbookFolder().getPath() + "/libraries/TimeAlarms/keywords.txt");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/TimeAlarms/readme.txt",Base.getSketchbookFolder().getPath() + "/libraries/TimeAlarms/readme.txt");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/README.md",Base.getSketchbookFolder().getPath() + "/libraries/README.md");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/AI/AI.cpp",Base.getSketchbookFolder().getPath() + "/libraries/AI/AI.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/AI/AI.h",Base.getSketchbookFolder().getPath() + "/libraries/AI/AI.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Globals/Globals.cpp",Base.getSketchbookFolder().getPath() + "/libraries/Globals/Globals.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Globals/Globals.h",Base.getSketchbookFolder().getPath() + "/libraries/Globals/Globals.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/InternalEEPROM/InternalEEPROM.cpp",Base.getSketchbookFolder().getPath() + "/libraries/InternalEEPROM/InternalEEPROM.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/InternalEEPROM/InternalEEPROM.h",Base.getSketchbookFolder().getPath() + "/libraries/InternalEEPROM/InternalEEPROM.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/IO/IO.cpp",Base.getSketchbookFolder().getPath() + "/libraries/IO/IO.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/IO/IO.h",Base.getSketchbookFolder().getPath() + "/libraries/IO/IO.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/LED/LED.cpp",Base.getSketchbookFolder().getPath() + "/libraries/LED/LED.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/LED/LED.h",Base.getSketchbookFolder().getPath() + "/libraries/LED/LED.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Memory/Memory.cpp",Base.getSketchbookFolder().getPath() + "/libraries/Memory/Memory.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Memory/Memory.h",Base.getSketchbookFolder().getPath() + "/libraries/Memory/Memory.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ORP/ORP.cpp",Base.getSketchbookFolder().getPath() + "/libraries/ORP/ORP.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ORP/ORP.h",Base.getSketchbookFolder().getPath() + "/libraries/ORP/ORP.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_ATO/RA_ATO.cpp",Base.getSketchbookFolder().getPath() + "/libraries/RA_ATO/RA_ATO.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_ATO/RA_ATO.h",Base.getSketchbookFolder().getPath() + "/libraries/RA_ATO/RA_ATO.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_Colors/RA_Colors.h",Base.getSketchbookFolder().getPath() + "/libraries/RA_Colors/RA_Colors.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_CustomColors/RA_CustomColors.h",Base.getSketchbookFolder().getPath() + "/libraries/RA_CustomColors/RA_CustomColors.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_Joystick/RA_Joystick.cpp",Base.getSketchbookFolder().getPath() + "/libraries/RA_Joystick/RA_Joystick.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_Joystick/RA_Joystick.h",Base.getSketchbookFolder().getPath() + "/libraries/RA_Joystick/RA_Joystick.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_NokiaLCD/README",Base.getSketchbookFolder().getPath() + "/libraries/RA_NokiaLCD/README");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_NokiaLCD/RA_NokiaLCD.cpp",Base.getSketchbookFolder().getPath() + "/libraries/RA_NokiaLCD/RA_NokiaLCD.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_NokiaLCD/RA_NokiaLCD.h",Base.getSketchbookFolder().getPath() + "/libraries/RA_NokiaLCD/RA_NokiaLCD.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_PWM/RA_PWM.cpp",Base.getSketchbookFolder().getPath() + "/libraries/RA_PWM/RA_PWM.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_PWM/RA_PWM.h",Base.getSketchbookFolder().getPath() + "/libraries/RA_PWM/RA_PWM.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_TempSensor/RA_TempSensor.cpp",Base.getSketchbookFolder().getPath() + "/libraries/RA_TempSensor/RA_TempSensor.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_TempSensor/RA_TempSensor.h",Base.getSketchbookFolder().getPath() + "/libraries/RA_TempSensor/RA_TempSensor.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_Wifi/RA_Wifi.cpp",Base.getSketchbookFolder().getPath() + "/libraries/RA_Wifi/RA_Wifi.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RA_Wifi/RA_Wifi.h",Base.getSketchbookFolder().getPath() + "/libraries/RA_Wifi/RA_Wifi.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefAngel/ReefAngel.cpp",Base.getSketchbookFolder().getPath() + "/libraries/ReefAngel/ReefAngel.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefAngel/ReefAngel.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefAngel/ReefAngel.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefAngel/README",Base.getSketchbookFolder().getPath() + "/libraries/ReefAngel/README");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefAngel/keywords.txt",Base.getSketchbookFolder().getPath() + "/libraries/ReefAngel/keywords.txt");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefAngel_Features/ReefAngel_Features.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefAngel_Features/ReefAngel_Features.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/Arial12.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/Arial12.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/ArialBold20.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/ArialBold20.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/ArialBold24.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/ArialBold24.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/f8x8.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/f8x8.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/f12x12.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/f12x12.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/f15x22.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/f15x22.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/Fat32.cpp",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/Fat32.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/Fat32.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/Fat32.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/Green.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/Green.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/mmc.cpp",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/mmc.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/mmc.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/mmc.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/Red.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/Red.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/ReefAngel_Logo.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/ReefAngel_Logo.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/ReefTouch.cpp",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/ReefTouch.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/ReefTouch.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/ReefTouch.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/SDFile.cpp",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/SDFile.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/SDFile.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/SDFile.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/ReefTouch/TestImage.h",Base.getSketchbookFolder().getPath() + "/libraries/ReefTouch/TestImage.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Relay/Relay.cpp",Base.getSketchbookFolder().getPath() + "/libraries/Relay/Relay.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Relay/Relay.h",Base.getSketchbookFolder().getPath() + "/libraries/Relay/Relay.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RF/RF.cpp",Base.getSketchbookFolder().getPath() + "/libraries/RF/RF.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RF/RF.h",Base.getSketchbookFolder().getPath() + "/libraries/RF/RF.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RT_PWM/RT_PWM.cpp",Base.getSketchbookFolder().getPath() + "/libraries/RT_PWM/RT_PWM.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/RT_PWM/RT_PWM.h",Base.getSketchbookFolder().getPath() + "/libraries/RT_PWM/RT_PWM.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Salinity/Salinity.cpp",Base.getSketchbookFolder().getPath() + "/libraries/Salinity/Salinity.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Salinity/Salinity.h",Base.getSketchbookFolder().getPath() + "/libraries/Salinity/Salinity.h");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Timer/Timer.cpp",Base.getSketchbookFolder().getPath() + "/libraries/Timer/Timer.cpp");
-	        AddFile("https://raw.github.com/reefangel/libraries/master/Timer/Timer.h",Base.getSketchbookFolder().getPath() + "/libraries/Timer/Timer.h");
-	        AddFile("http://www.reefangel.com/update/files/RA_Preloaded/RA_Preloaded.ino",Base.getSketchbookFolder().getPath() + "/RA_Preloaded/RA_Preloaded.ino");
-	        AddFile("http://www.reefangel.com/update/files/tools/RALibsVer/tool/RALibsVer.jar",Base.getSketchbookFolder().getPath() + "/tools/RALibsVer/tool/RALibsVer.jar");
-	        AddFile("http://www.reefangel.com/update/files/tools/RestorePreloaded/tool/RestorePreloaded.jar",Base.getSketchbookFolder().getPath() + "/tools/RestorePreloaded/tool/RestorePreloaded.jar");
-	        AddFile("http://www.reefangel.com/update/files/tools/RestorePreloaded/data/connection.png",Base.getSketchbookFolder().getPath() + "/tools/RestorePreloaded/data/connection.png");
-	        AddFile("http://www.reefangel.com/update/files/tools/RestorePreloaded/data/ra_small.png",Base.getSketchbookFolder().getPath() + "/tools/RestorePreloaded/data/ra_small.png");
-	        AddFile("http://www.reefangel.com/update/files/tools/Wizard/tool/Wizard.jar",Base.getSketchbookFolder().getPath() + "/tools/Wizard/tool/Wizard.jar");
-	        AddFile("http://www.reefangel.com/update/files/tools/Wizard/data/AI.png",Base.getSketchbookFolder().getPath() + "/tools/Wizard/data/AI.png");
-	        AddFile("http://www.reefangel.com/update/files/tools/Wizard/data/connection.png",Base.getSketchbookFolder().getPath() + "/tools/Wizard/data/connection.png");
-	        AddFile("http://www.reefangel.com/update/files/tools/Wizard/data/moon.png",Base.getSketchbookFolder().getPath() + "/tools/Wizard/data/moon.png");
-	        AddFile("http://www.reefangel.com/update/files/tools/Wizard/data/parabola.png",Base.getSketchbookFolder().getPath() + "/tools/Wizard/data/parabola.png");
-	        AddFile("http://www.reefangel.com/update/files/tools/Wizard/data/parabolasettings.png",Base.getSketchbookFolder().getPath() + "/tools/Wizard/data/parabolasettings.png");
-	        AddFile("http://www.reefangel.com/update/files/tools/Wizard/data/ra_ato.png",Base.getSketchbookFolder().getPath() + "/tools/Wizard/data/ra_ato.png");
-	        AddFile("http://www.reefangel.com/update/files/tools/Wizard/data/ra_dimming.png",Base.getSketchbookFolder().getPath() + "/tools/Wizard/data/ra_dimming.png");
-	        AddFile("http://www.reefangel.com/update/files/tools/Wizard/data/ra_small.png",Base.getSketchbookFolder().getPath() + "/tools/Wizard/data/ra_small.png");
-	        AddFile("http://www.reefangel.com/update/files/tools/Wizard/data/Radion.png",Base.getSketchbookFolder().getPath() + "/tools/Wizard/data/Radion.png");
-	        AddFile("http://www.reefangel.com/update/files/tools/Wizard/data/relay_small.png",Base.getSketchbookFolder().getPath() + "/tools/Wizard/data/relay_small.png");
-	        AddFile("http://www.reefangel.com/update/files/tools/Wizard/data/slope.png",Base.getSketchbookFolder().getPath() + "/tools/Wizard/data/slope.png");
-	        AddFile("http://www.reefangel.com/update/files/tools/Wizard/data/slopesettings.png",Base.getSketchbookFolder().getPath() + "/tools/Wizard/data/slopesettings.png");
+			try
+			{
+				copyURLtoFile("http://www.reefangel.com/update/files/update/source.txt",Base.getSketchbookFolder().getPath() + "/update/source.txt");
+				FileReader fileReader = new FileReader(Base.getSketchbookFolder().getPath() + "/update/source.txt"); 
+				BufferedReader bufferedReader = new BufferedReader(fileReader); 
+				List<String> lines = new ArrayList<String>(); 
+				String line = null; 
+				while ((line = bufferedReader.readLine()) != null) { 
+					lines.add(line); 
+				} 
+				bufferedReader.close(); 
+				SourceFiles=lines.toArray(new String[lines.size()]); 
+			}
+			catch (IOException e1) {
+				e1.printStackTrace();
+			} 	
 
 	        for (int a=0;a<SourceFiles.length;a++)
 	        {
-	        	copyURLtoFile(SourceFiles[a],DestFiles[a]);
+	        	String s=SourceFiles[a].replace("http://www.reefangel.com/update/files", "").replace("https://raw.github.com/reefangel/libraries/master", "/libraries");
+	        	System.out.println("Updating " + s);
+	        	copyURLtoFile(SourceFiles[a],Base.getSketchbookFolder().getPath() + s);
 	        	progressBar.setValue(a);
 	        }
 	        dialog.dispose();
@@ -336,20 +252,6 @@ public class UpdateCheck implements Runnable {
 			}
 		}
 		
-		public void AddFile(String source, String dest)
-		{
-			SourceFiles = push(SourceFiles,source);
-			DestFiles = push(DestFiles,dest);
-			progressBar.setMaximum(SourceFiles.length);
-//			progressBar.repaint();
-		}
-		
-		private String[] push(String[] array, String push) { 
-		    String[] longer = new String[array.length + 1]; 
-		    System.arraycopy(array, 0, longer, 0, array.length); 
-		    longer[array.length] = push; 
-		    return longer; 
-		} 
 		private void copyURLtoFile (String origin, String dest){
 			URL originURL = null;
 			
